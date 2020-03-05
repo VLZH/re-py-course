d064d158a77258275ca04445b3026bd40eb503d8
vlzh <vlzh@pm.me>
2020-03-05 18:29:06 +0600

    New section 'DB'

--- sidebars.js
+++ sidebars.js
@@ -1,30 +1,29 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 module.exports = {
-  docsSidebar: {
-    Общее: ["intro", "general/languages"],
-    Python: [
-      "python/syntax",
-      "python/operators",
-      "python/statements",
-      "python/functions",
-      "python/basic_types",
-      "python/fp",
-      "python/modules_and_packets",
-      "python/classes"
-    ],
-    Модули: ["modules/os", "modules/re"],
-    Дополнительное: [
-      "additional/telegram_bot_workshop",
-      "additional/git",
-      "additional/recommended",
-      "additional/protocols"
-    ],
-    Домашки: ["homeworks/farm"]
-  }
+    docsSidebar: {
+        Общее: ["intro", "general/languages"],
+        Python: [
+            "python/syntax",
+            "python/operators",
+            "python/statements",
+            "python/functions",
+            "python/basic_types",
+            "python/fp",
+            "python/modules_and_packets",
+            "python/classes"
+        ],
+        Модули: ["modules/os", "modules/re"],
+        Дополнительное: [
+            "additional/telegram_bot_workshop",
+            "additional/git",
+            "additional/recommended",
+            "additional/protocols",
+            "additional/wsgi",
+            {
+                type: "category",
+                label: "DB",
+                items: ["additional/db/db", "additional/db/sql"]
+            }
+        ],
+        Домашки: ["homeworks/farm"]
+    }
 };
--- src/theme/Link.js
+++ src/theme/Link.js
@@ -1,19 +1,26 @@
 import React from "react";
 import "./Link.css";
 
-export const Link = ({ href, title, subtitle, caption }) => {
-  return (
-    <div className="mdx_link_wrapper">
-      <a href={href} className="mdx_link">
-        <span className="mdx_link_title">{title}</span>
-        {subtitle ? (
-          <span className="mdx_link_subtitle">{subtitle}</span>
-        ) : null}
-        <span className="mdx_link_href">{href}</span>
-      </a>
-      <span className="mdx_link_cation">{caption}</span>
-    </div>
-  );
+export const Link = ({ href, title, subtitle, caption, image }) => {
+    return (
+        <div className="mdx_link_wrapper">
+            <a href={href} className="mdx_link">
+                <div className="mdx_link_left">
+                    <span className="mdx_link_title">{title}</span>
+                    {subtitle ? (
+                        <span className="mdx_link_subtitle">{subtitle}</span>
+                    ) : null}
+                    <span className="mdx_link_href">{href}</span>
+                </div>
+                {image ? (
+                    <div className="mdx_link_right">
+                        <img className="mdx_link_image" src={image} />
+                    </div>
+                ) : null}
+            </a>
+            <span className="mdx_link_cation">{caption}</span>
+        </div>
+    );
 };
 
 export default Link;
