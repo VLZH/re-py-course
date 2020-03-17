b115bbc25359d825c2d67da8ed48ac16fd5068e1
vlzh <vlzh@pm.me>
2020-03-17 17:26:04 +0600

    Init new page about 'Flask'

--- sidebars.js
+++ sidebars.js
@@ -1,29 +1,34 @@
 module.exports = {
-    docsSidebar: {
-        Общее: ["intro", "general/languages"],
-        Python: [
-            "python/syntax",
-            "python/operators",
-            "python/statements",
-            "python/functions",
-            "python/basic_types",
-            "python/fp",
-            "python/modules_and_packets",
-            "python/classes"
-        ],
-        Модули: ["modules/os", "modules/re"],
-        Дополнительное: [
-            "additional/telegram_bot_workshop",
-            "additional/git",
-            "additional/recommended",
-            "additional/protocols",
-            "additional/wsgi",
-            {
-                type: "category",
-                label: "DB",
-                items: ["additional/db/db", "additional/db/sql"]
-            }
-        ],
-        Домашки: ["homeworks/farm", "homeworks/linkshorter"]
-    }
+  docsSidebar: {
+    Общее: ["intro", "general/languages"],
+    Python: [
+      "python/syntax",
+      "python/operators",
+      "python/statements",
+      "python/functions",
+      "python/basic_types",
+      "python/fp",
+      "python/modules_and_packets",
+      "python/classes",
+      "python/pip"
+    ],
+    Модули: ["modules/os", "modules/re"],
+    Дополнительное: [
+      "additional/telegram_bot_workshop",
+      "additional/git",
+      "additional/recommended",
+      "additional/protocols",
+      "additional/wsgi",
+      {
+        type: "category",
+        label: "DB",
+        items: ["additional/db/db", "additional/db/sql"]
+      }
+    ],
+    Фреймворки: [
+        "flask/intro",
+        "flask/hello_world",
+    ],
+    Домашки: ["homeworks/farm", "homeworks/linkshorter"]
+  }
 };
