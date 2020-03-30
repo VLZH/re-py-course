1363fd41a15acddb17656617f2533a6f6e9f9c62
iden <idenmart07@gmail.com>
2020-03-30 23:34:48 +0600

    mydatabse

--- sidebars.js
+++ sidebars.js
@@ -29,6 +29,6 @@ module.exports = {
         "flask/intro",
         "flask/hello_world",
     ],
-    Домашки: ["homeworks/farm", "homeworks/linkshorter", "homeworks/blog"]
+    Домашки: ["homeworks/farm", "homeworks/linkshorter", "homeworks/blog" ,"homeworks/mydatabase"]
   }
 };
